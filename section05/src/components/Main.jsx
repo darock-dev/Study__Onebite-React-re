5e910d1e3c95317f93badfe0cc576c78e46f0607
Dova <heetask@gmail.com>
2026-08-07 09:46:18 +0900

    5.3) JSX로 UI 표현하기

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,17 @@
+// JSX 주의사항
+// 1. 중괄호 내부에는 자바스크립트 표현식만 사용 가능
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다
+// 3. 모든 태그는 닫혀있어야 한다.
+// 4. 취상위 태그는 반드시 하나
+
+import "./Main.css";
+
 const Main = () => {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+  const user = {
+    name: "Dova",
+    isLogin: true,
+  };
+  return <>{user.isLogin ? <div className="logout">로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
